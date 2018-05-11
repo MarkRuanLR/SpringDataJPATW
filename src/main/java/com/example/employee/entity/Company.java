09d220df67589bba1f333a6386ff179e7ee73b4b
strawls <mark_ruan@icloud.com>
2018-05-11 20:42:17 +0800

    finish expect practice 5 which cause a error from Mysql. The error is "Unknown column 'company0_.companyId' in 'where clause'"

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,54 @@
 package com.example.employee.entity;
 
-public class Company {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
+public class Company {
+    @Id
+    private int companyId;
+    private String companyName;
+    private int numberOfEmployees;
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "companyId=" + companyId +
+                ", companyName='" + companyName + '\'' +
+                ", numberOfEmployees=" + numberOfEmployees +
+                '}';
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public Company(int companyId, String companyName, int numberOfEmployees) {
+
+        this.companyId = companyId;
+        this.companyName = companyName;
+        this.numberOfEmployees = numberOfEmployees;
+    }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,63 @@
 package com.example.employee.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Employee {
 
+    private String name;
+    @Id
+    private int id;
+    private String gender;
+    private int salary;
+    private int age;
+    private int companyId;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int id, String gender, int salary, int age, int companyId) {
+        this.name = name;
+        this.id = id;
+        this.gender = gender;
+        this.salary = salary;
+        this.age = age;
+        this.companyId = companyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", gender='" + gender + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                ", companyId=" + companyId +
+                '}';
+    }
 }
